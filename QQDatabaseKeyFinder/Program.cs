09965887339fdbdcab9771c46229b073b2d6f731
ilyfairy <me@ilyfairy.com>
2025-10-16 23:40:03 +0800

    自动找qq

--- QQDatabaseKeyFinder/Program.cs
+++ QQDatabaseKeyFinder/Program.cs
@@ -1,33 +1,49 @@
 using System.Text;
+using Microsoft.Win32;
 using QQDatabaseKeyFinder;
 
 
 Console.OutputEncoding = Encoding.UTF8;
 
-HashSet<string> qqFilePaths =
-[
-    Path.Combine(Environment.GetEnvironmentVariable("ProgramFiles") ?? @"C:\Program Files", "Tencent", "QQNT", "QQ.exe"),
-            @"C:\Program Files\Tencent\QQNT\QQ.exe",
-            @"D:\Program Files\Tencent\QQNT\QQ.exe",
-        ];
-string? qqntFilePath = null;
+HashSet<string> qqFilePaths = new();
+string? qqFilePath = null;
+
+if (OperatingSystem.IsWindows())
+{
+    try
+    {
+        if (Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Tencent\QQNT") is { } qqNTRegistry)
+        {
+            if (qqNTRegistry.GetValue("Install") is string qqInstallDirectory)
+            {
+                qqFilePaths.Add(Path.Combine(qqInstallDirectory, "QQ.exe"));
+            }
+        }
+    }
+    catch { }
+
+    qqFilePaths.Add(Path.Combine(Environment.GetEnvironmentVariable("ProgramFiles") ?? @"C:\Program Files", "Tencent", "QQNT", "QQ.exe"));
+    qqFilePaths.Add(@"C:\Program Files\Tencent\QQNT\QQ.exe");
+    qqFilePaths.Add(@"D:\Program Files\Tencent\QQNT\QQ.exe");
+}
+
 foreach (var item in qqFilePaths)
 {
     if (File.Exists(item))
     {
-        qqntFilePath = item;
+        qqFilePath = item;
         break;
     }
 }
 
-if (qqntFilePath == null)
+if (qqFilePath == null)
 {
     throw new Exception("没有找到QQ.exe");
 }
 
 Console.WriteLine("等待QQ登录...");
 
-var key = QQDebugger.NewProcess(qqntFilePath);
+var key = QQDebugger.NewProcess(qqFilePath);
 if (key is null)
 {
     Console.WriteLine("没有找到key");
